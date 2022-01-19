0e9c97cca9ab63ecb141cedb8e39b3d887cef1fd
naufalahmadfauz <supernaufalboy@gmail.com>
2022-01-20 00:23:28 +0700

    added free sql

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$nama_database = "perpustakaan";
+$server = "sql6.freesqldatabase.com";
+$user = "sql6466722";
+$password = "QQFLQ6u6Ap";
+$nama_database = "sql6466722";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
